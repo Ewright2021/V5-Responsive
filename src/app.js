3f086a8a23c9023b0b1cf818e83e8c747b4c0c06
Erika Wright <hey@ewright.me>
2021-02-25 11:28:13 -0500

    updates to promotion cards and login form

--- src/app.js
+++ src/app.js
@@ -252,3 +252,10 @@ function fillScreenTitle() {
 }
 
 fillScreenTitle();
+
+//  Truncate Shopping Title
+function truncateString(str, num) {
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
